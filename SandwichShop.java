18ad9a58103f655cdd25f2ac706454a1a2086b8f
yunghobbits <mccowen64@gmail.com>
2017-08-01 21:41:53 -0500

    Update SandwichShop.java

--- SandwichShop.java
+++ SandwichShop.java
@@ -13,7 +13,7 @@ public class SandwichShop
         // Your code goes below here.
         out.println("Checking sales goals.");
         //veggie
-        out.println("The sales goal for veggie sandwiches is 50");
+        out.println("The sales goal for veggie sandwiches is " + goalForVeggies);
         out.println("How many veggie sandwiches were sold today?");
         int vegSold = keyboard.nextInt();
         out.println("");
@@ -27,7 +27,7 @@ public class SandwichShop
             out.println("Fell short for veggies.");
         }
         //burger
-        out.println("The sales goal for burgers is 250");
+        out.println("The sales goal for burgers is " + goalForBurgers);
         out.println("How many burgers were sold today?");
         int burgSold = keyboard.nextInt();
         out.println("");
@@ -41,7 +41,7 @@ public class SandwichShop
             out.println("Fell short for burgers.");
         }
         //subs
-        out.println("The sales goal for subs is 180");
+        out.println("The sales goal for subs is "+ goalForSubs);
         out.println("How many subs were sold today?");
         int subsSold = keyboard.nextInt();
         out.println("");
@@ -55,7 +55,7 @@ public class SandwichShop
             out.println("Fell short for subs.");
         }
         //soup
-        out.println("The sales goal for soup is 70");
+        out.println("The sales goal for soup is " + goalForSoup);
         out.println("How many soups were sold today?");
         int soupSold = keyboard.nextInt();
         out.println("");
